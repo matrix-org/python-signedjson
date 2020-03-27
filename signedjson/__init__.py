988688cd390b5d8b90fe49d44ea4912b8170e8df
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2020-03-27 19:39:23 +0000

    Use setuptools_scm for version number (#7)

--- signedjson/__init__.py
+++ signedjson/__init__.py
@@ -12,4 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.1.0"
+from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: nocover
+    # package is not installed
+    pass
