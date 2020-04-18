03b4c256e184837d3ed251a56bd3e3f1d9e5d4a1
MeggyCal <MeggyCal@users.noreply.github.com>
2020-04-18 23:02:56 +0200

    and now?

--- signedjson/__init__.py
+++ signedjson/__init__.py
@@ -14,7 +14,7 @@
 
 try:
     from importlib.metadata import version, PackageNotFoundError
-except ImportError: # pragma: no cover
+except ImportError: # pragma: nocover
     from importlib_metadata import version, PackageNotFoundError
 
 try:
