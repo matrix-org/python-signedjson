cb7208bd706d6f61795340c4d73ec931daf0d2ab
Markéta Calábková <calabkovam@fykos.cz>
2020-04-20 15:29:23 +0200

    two spaces

--- signedjson/__init__.py
+++ signedjson/__init__.py
@@ -14,7 +14,7 @@
 
 try:
     from importlib.metadata import version, PackageNotFoundError
-except ImportError: # pragma: nocover
+except ImportError:  # pragma: nocover
     from importlib_metadata import version, PackageNotFoundError
 
 try:
